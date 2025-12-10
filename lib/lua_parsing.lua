75e836266d2616191a2c45876c5bdf4ca7344424
felipe bernardinello <felipebernardinello@gmail.com>
2025-12-10 13:58:15 -0300

    new resume

---
+++ lib/lua_parsing.lua
@@ -0,0 +1,24 @@
+-- Helper function to parse key=value
+function parse_keyval(str)
+    local result = {}
+    str = str:gsub("^%s+", ""):gsub("%s+$", "")
+
+    for key, value in str:gmatch("(%w+)%s*=%s*{([^}]*)}") do
+        result[key] = value
+    end
+    
+    return result
+end
+
+resume_data = {}
+
+function set_resume_data(category, kvstr)
+    resume_data[category] = parse_keyval(kvstr)
+end
+
+function get_resume_value(category, key, default)
+    if resume_data[category] and resume_data[category][key] then
+        return resume_data[category][key]
+    end
+    return default or ""
+end
